75866cdccf511a66086685a8d4f69f70f384a50d
shawn <shawn.lee@tosslab.com>
2020-07-06 19:13:17 +0900

    최솟값 알고리즘 풀이

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -2,8 +2,23 @@ class SlackController < ApplicationController
   protect_from_forgery with: :null_session
 
   def index
-    client = Slack::Web::Client.new
-    logger.info client.auth_test
+    arr_a = [1, 4, 2]
+    arr_b = [4, 5, 3]
+
+    sorted_arr_a = arr_a.sort
+    sorted_arr_b = arr_b.sort.reverse
+
+    res = 0
+    sorted_arr_a.each_with_index do |a_node, idx|
+      res += a_node * sorted_arr_b[idx]
+    end
+
+    render json: {
+        sorted_arr_a: sorted_arr_a,
+        sorted_arr_b: sorted_arr_b,
+        res: res
+    }
+
   end
 
   def create
